3a4a4ed107c5027cfe0ccf290204fd4bdc88a478
Jeremy Low <jrmylow@gmail.com>
2024-06-05 00:08:36 +1000

    exercises

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,7 +8,9 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
-#[derive(Copy, Clone, PartialEq, Debug)]
+use core::ops::Add;
+
+#[derive(Copy, Clone, Debug)]
 pub struct SaturatingU16 {
     value: u16,
 }
@@ -16,22 +18,30 @@ pub struct SaturatingU16 {
 // Conversions
 impl From<u16> for SaturatingU16 {
     fn from(value: u16) -> Self {
-
+        SaturatingU16 {
+            value: value,
+        }
     }
 }
 impl From<&u16> for SaturatingU16 {
     fn from(value: &u16) -> Self {
-        
+        SaturatingU16 {
+            value: *value,
+        }        
     }
 }
 impl From<u8> for SaturatingU16 {
     fn from(value: u8) -> Self {
-        
+        SaturatingU16 {
+            value: u16::from(value),
+        }       
     }
 }
 impl From<&u8> for SaturatingU16 {
     fn from(value: &u8) -> Self {
-
+        SaturatingU16 {
+            value: u16::from(*value),
+        }   
     }
 }
 
@@ -39,40 +49,48 @@ impl From<&u8> for SaturatingU16 {
 impl Add<SaturatingU16> for SaturatingU16 {
     type Output = SaturatingU16;
 
-    fn add(self, other) -> SaturatingU16 {
-
+    fn add(self, other: SaturatingU16) -> SaturatingU16 {
+        SaturatingU16 {
+            value: self.value.saturating_add(other.value),
+        }
     }
 }
 impl Add<&SaturatingU16> for SaturatingU16 {
     type Output = SaturatingU16;
 
-    fn add(self, other) -> SaturatingU16 {
-
+    fn add(self, other: &SaturatingU16) -> SaturatingU16 {
+        SaturatingU16 {
+            value: self.value.saturating_add(other.value),
+        }
     }
 }
 impl Add<u16> for SaturatingU16 {
     type Output = SaturatingU16;
 
-    fn add(self, other) -> SaturatingU16 {
-
+    fn add(self, other: u16) -> SaturatingU16 {
+        SaturatingU16 {
+            value: self.value.saturating_add(other),
+        }
     }
 }
 impl Add<&u16> for SaturatingU16 {
     type Output = SaturatingU16;
 
-    fn add(self, other) -> SaturatingU16 {
-
+    fn add(self, other: &u16) -> SaturatingU16 {
+        SaturatingU16 {
+            value: self.value.saturating_add(*other),
+        }
     }
 }
 
 // Comparisons
 impl PartialEq<SaturatingU16> for SaturatingU16 {
     fn eq(&self, other: &SaturatingU16) -> bool {
-
+        self.value == other.value
     }
 }
 impl PartialEq<u16> for SaturatingU16 {
     fn eq(&self, other: &u16) -> bool {
-        
+        self.value == *other
     }
 }
--- exercises/05_ticket_v2/01_enum/src/lib.rs
+++ exercises/05_ticket_v2/01_enum/src/lib.rs
@@ -1,21 +1,33 @@
 // TODO: use `Status` as type for `Ticket::status`
 //   Adjust the signature and implementation of all other methods as necessary.
 
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum Status {
+    // TODO: add the missing variants
+    ToDo,
+    InProgress,
+    Done,
+}
+
 #[derive(Debug, PartialEq)]
 // `derive`s are recursive: it can only derive `PartialEq` if all fields also implement `PartialEq`.
 // Same holds for `Debug`. Do what you must with `Status` to make this work.
 struct Ticket {
     title: String,
     description: String,
-    status: String,
+    status: Status,
 }
 
-enum Status {
-    // TODO: add the missing variants
+impl PartialEq<&str> for Status {
+    fn eq(&self, other: &&str) -> bool {
+        ((*self == Status::ToDo) & (*other == "To-Do")) |
+        ((*self == Status::InProgress) & (*other == "In Progress")) |
+        ((*self == Status::Done) & (*other == "Done"))
+    }
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
+    pub fn new(title: String, description: String, status: Status) -> Ticket {
         if title.is_empty() {
             panic!("Title cannot be empty");
         }
@@ -47,7 +59,7 @@ impl Ticket {
         &self.description
     }
 
-    pub fn status(&self) -> &String {
+    pub fn status(&self) -> &Status {
         &self.status
     }
 }
